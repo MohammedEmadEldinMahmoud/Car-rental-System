9a7436cec650bf36f8c3074760225e512e0c17cf
MohammedEmadEldinMahmoud <mohamedemadeldinmahmoud@gmail.com>
2024-01-01 16:17:02 +0200

    update erd,schema image

--- DBMS/DDL.sql
+++ DBMS/DDL.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Offices (
     office_id INT AUTO_INCREMENT PRIMARY KEY,
     location varchar(255) not null,
-     contact_info VARCHAR(255) not null
+    contact_info VARCHAR(255) not null
 
 );
 CREATE TABLE Customers (
@@ -21,8 +21,7 @@ CREATE TABLE Reservations(
     ReservationDate DATE,
     PickupDate DATE,
     ReturnDate DATE,
-        CustomerID INT,
-
+    CustomerID INT,
     FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 
 );
@@ -37,7 +36,7 @@ CREATE TABLE Payments (
 
 
 CREATE TABLE Cars (
-        CarID INT AUTO_INCREMENT PRIMARY KEY,
+    CarID INT AUTO_INCREMENT PRIMARY KEY,
     PlateID VARCHAR(20),
     Status VARCHAR(20),
     Year INT,
